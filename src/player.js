766a1b743d1a7c6fc8994d095b9764df90a64c0a
thyshimrod <shimrod@free.fr>
2016-04-26 20:11:55 +0200

    add array of level attribute to player

--- src/player.js
+++ src/player.js
@@ -1,18 +1,20 @@
 'use strict';
 
 (function (client) {
-    
+
     var player = function () { };
-    
+
     if (typeof module === "object" && module && typeof module.exports === "object") {
         module.exports = player;
     }
     else {
         client.player = player;
     }
-    
+
     player.prototype.id = null;
     player.prototype.name = null;
     player.prototype.character = null;
+    player.prototype.level = null; //array of levels (old and actual)
+    player.prototype.actualLevel = null; //int : on which level character is (it can down or up back)
 
 })(this);
