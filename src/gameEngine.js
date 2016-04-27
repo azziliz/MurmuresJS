4e0f81f690eb458b59ca0b019b7dfa53717d7939
azziliz <chriskang@free.fr>
2016-04-28 00:27:20 +0200

    partial game engine

---
+++ src/gameEngine.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+(function (client) {
+    
+    var gameEngine = function () {
+        /// <field name="tileSize" type="Number"/>
+        /// <field name="level" type="level"/>
+        /// <field name="hero" type="character"/>
+        /// <field name="mobs" type="character"/>
+    };
+    
+    if (typeof module === "object" && module && typeof module.exports === "object") {
+        module.exports = gameEngine;
+    }
+    else {
+        client.gameEngine = gameEngine;
+    }
+    
+    gameEngine.prototype.fromJson = function (src) {
+        /// <param name="src" type="gameEngine"/>
+        this.tileSize = src.tileSize;
+        this.id = src.id;
+        this.layout = src.layout;
+        this.width = src.width;
+        this.height = src.height;
+        this.tiles = src.tiles;
+        this.hero = src.hero;
+    };
+
+})(this);
---
+++ src/order.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+(function (client) {
+    
+    var order = function () {
+        /// <field name="type" type="String"/>
+        /// <field name="target" type="tile"/>
+    };
+    
+    if (typeof module === "object" && module && typeof module.exports === "object") {
+        module.exports = order;
+    }
+    else {
+        client.order = order;
+    }
+    
+    order.prototype.fromJson = function (src) {
+        /// <param name="src" type="order"/>
+        this.type = src.type;
+        this.target = src.target;
+    };
+
+})(this);
