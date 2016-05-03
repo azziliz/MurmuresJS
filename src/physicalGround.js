fad4c7b0346654f51efa438df6f9f6b88c8778f0
azziliz <chriskang@free.fr>
2016-05-03 21:04:45 +0200

    testing various classes

---
+++ src/physicalGround.js
@@ -0,0 +1,11 @@
+﻿
+
+var physicalGround = function () {
+    this.uniqueId = '';
+    this.tileset = '';
+    this.tilesetReference = '';
+    this.allowFlying = false;
+    this.allowTerrestrial = false;
+    this.allowAquatic = false;
+    this.allowUnderground = false;
+};
