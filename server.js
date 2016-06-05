9505775d3770b5ac1ac9b6b230e27a2b4ba67201
azziliz <chriskang@free.fr>
2016-06-06 01:58:26 +0200

    random heroes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var murmures = {
      * @private
      */
     startTime: process.hrtime(),
-
+    
     /**
      * Writes timestamped log to the console.
      * @public
@@ -30,7 +30,7 @@ var murmures = {
         }
         return fdiff;
     },
-
+    
     restartGame: function (targetLevel) {
         gameEngine.levels = [];
         gameEngine.activeLevel = 0;
@@ -48,25 +48,31 @@ var murmures = {
             loopCounter++;
         }, this);
         gameEngine.level = gameEngine.levels[gameEngine.activeLevel];
-
-        let hero1Txt = fs.readFileSync('./data/hero1.json', 'utf8').toString().replace(/^\uFEFF/, '');
+        
         gameEngine.heros = [];
-        let hero = new murmures.Character();
-        hero.build(gameEngine.level.getStartingPoint(), JSON.parse(hero1Txt).mobTemplate);
-        hero.setVision();
-        hero.stateOrder = murmures.C.STATE_HERO_ORDER_INPROGRESS;
-        gameEngine.heros.push(hero);
-
-        let hero2Txt = fs.readFileSync('./data/hero2.json', 'utf8').toString().replace(/^\uFEFF/, '');
-        let hero2 = new murmures.Character();
-        hero2.build(gameEngine.level.getStartingPoint(), JSON.parse(hero2Txt).mobTemplate);
-        gameEngine.heros.push(hero2);
-
-        let hero3Txt = fs.readFileSync('./data/hero3.json', 'utf8').toString().replace(/^\uFEFF/, '');
-        let hero3 = new murmures.Character();
-        hero3.build(gameEngine.level.getStartingPoint(), JSON.parse(hero3Txt).mobTemplate);
-        gameEngine.heros.push(hero3);
-
+        let allHeroesKeys = [];
+        for (let assetId in gameEngine.bodies) {
+            let ref = gameEngine.bodies[assetId];
+            if (murmures.C.LAYERS[ref.layerId][0] === 'Hero') allHeroesKeys.push(assetId);
+        }
+        
+        let chosenHeroesKeys = [];
+        let chosenHero;
+        for (loopCounter = 0; loopCounter < 3; loopCounter++) {
+            do {
+                let rand = Math.floor(Math.random() * allHeroesKeys.length);
+                chosenHero = allHeroesKeys[rand];
+            } while (chosenHeroesKeys.indexOf(chosenHero) >= 0); // This loop prevents duplicate heroes
+            
+            let hero1 = new murmures.Character();
+            hero1.build(gameEngine.level.getStartingPoint(), chosenHero);
+            if (loopCounter === 0) {
+                hero1.setVision();
+                hero1.stateOrder = murmures.C.STATE_HERO_ORDER_INPROGRESS;
+            }
+            gameEngine.heros.push(hero1);
+        }
+                
         gameEngine.state = murmures.C.STATE_ENGINE_INIT;
     }
 };
@@ -90,14 +96,14 @@ murmures.serverLog('Loading classes');
         gameEngine: {}
     };
     vm.createContext(ctx);
-
+    
     let constantsjs = fs.readFileSync('./src/js/core/constants.js', 'utf8').toString().replace(/^\uFEFF/, '');
     vm.runInContext(constantsjs, ctx, { filename: 'constants.js' });
     let gameEnginejs = fs.readFileSync('./src/js/core/gameengine.js', 'utf8').toString().replace(/^\uFEFF/, '');
     vm.runInContext(gameEnginejs, ctx, { filename: 'gameengine.js' });
-
+    
     ctx.gameEngine = new murmures.GameEngine();
-
+    
     let playerjs = fs.readFileSync('./src/js/core/player.js', 'utf8').toString().replace(/^\uFEFF/, '');
     vm.runInContext(playerjs, ctx, { filename: 'player.js' });
     let tilejs = fs.readFileSync('./src/js/core/tile.js', 'utf8').toString().replace(/^\uFEFF/, '');
@@ -112,12 +118,12 @@ murmures.serverLog('Loading classes');
     vm.runInContext(physicalBodyjs, ctx, { filename: 'physicalbody.js' });
     let behaviorjs = fs.readFileSync('./src/js/core/behavior.js', 'utf8').toString().replace(/^\uFEFF/, '');
     vm.runInContext(behaviorjs, ctx, { filename: 'behavior.js' });
-
+    
     let vmperfjs = fs.readFileSync('./src/js/test/vmperf.js', 'utf8').toString().replace(/^\uFEFF/, '');
     vm.runInContext(vmperfjs, ctx, { filename: 'vmperf.js' });
     let servertestjs = fs.readFileSync('./src/js/test/servertest.js', 'utf8').toString().replace(/^\uFEFF/, '');
     vm.runInContext(servertestjs, ctx, { filename: 'servertest.js' });
-
+    
     gameEngine = ctx.gameEngine;
 })();
 
@@ -129,13 +135,13 @@ murmures.serverLog('Initializing game');
 (function () {
     let assetsJson = fs.readFileSync('./data/reference/assets.json', 'utf8').toString().replace(/^\uFEFF/, '');
     gameEngine.bodies = JSON.parse(assetsJson);
-
+    
     let localefrJson = fs.readFileSync('./data/locale/fr.json', 'utf8').toString().replace(/^\uFEFF/, '');
     let localeenJson = fs.readFileSync('./data/locale/en.json', 'utf8').toString().replace(/^\uFEFF/, '');
     gameEngine.locale = {};
     gameEngine.locale.fr = JSON.parse(localefrJson);
     gameEngine.locale.en = JSON.parse(localeenJson);
-
+    
     murmures.restartGame();
 })();
 
@@ -227,7 +233,7 @@ wss.on('connection', function (ws) {
                     let beforeState = gameEngine.clone(); // TODO : clone AFTER the turn.
                     murmures.serverLog('State saved');
                     gameEngine.saveOrder(clientOrder);
-
+                    
                     murmures.serverLog('Order applied');
                     let ge = gameEngine.compare(beforeState);
                     let res = JSON.stringify({ fn: 'o', payload: ge });
