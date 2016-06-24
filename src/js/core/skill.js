dae26d4f535a5aaa2118a10e188b2d604a1910c0
azziliz <chriskang@free.fr>
2016-06-24 14:44:26 +0200

    typo

--- src/js/core/skill.js
+++ src/js/core/skill.js
@@ -2,7 +2,7 @@
 //debugger;
 
 /**
- * @file Tile class. Part of the MurmuresJS project.
+ * @file Skill class. Part of the MurmuresJS project.
  * @author github.com/azziliz
  * @author github.com/thyshimrod
  * @license MIT
@@ -14,7 +14,7 @@
  * This custom properties are the specific part of skill, to manage a single class, with few different attributes between skills
  * An attack skill will have a damage custom attributes, whereas a healing skill, will have a heal custom attributes.
  *
- * A skill has has two behavior function, one, which will check if the skill can be applied, second which will be applied in case of validation
+ * A skill has two behavior function, one, which will check if the skill can be applied, second which will be applied in case of validation
  *
  * The targetAudience is defined if the skill can be applied on a mob, hero, (item) or everything
  *
@@ -22,30 +22,30 @@
  */
 
 murmures.Skill = function () {
+      /** @type {number} */
   this.id  = '';
+      /** @type {string} */
   this.name = '';
+      /** @type {Object.<string, Object.<string, string>>} */
   this.skillbehavior = {};
+      /** @type {Object} */
   this.custom = {};
+      /** @type {number} */
   this.range = 0;
+      /** @type {number} */
   this.targetaudience = 0;
+      /** @type {string} */
   this.asset = "";
 };
 
 murmures.Skill.prototype = {
     build : function (src,name) {
-      /** @type {number} */
       this.id = src.id;
-      /** @type {string} */
       this.name = name;
-      /** @type {Object.<string, Object.<string, string>>} */
       this.skillbehavior = src.skillbehavior;
-      /** @type {Object.<string, Object.<string, string>>} */
       this.custom = src.custom;
-      /** @type {number} */
       this.range = src.range;
-      /** @type {number} */
       this.targetaudience = src.targetaudience;
-      /** @type {string} */
       this.asset = src.asset;
     }
 };
