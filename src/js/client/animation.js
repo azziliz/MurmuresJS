812b969e74502220db91487125f2a32e5ea601ff
thyshimrod <thyshimrod@gmail.com>
2017-06-28 08:08:07 +0200

    update animation.js to be compliant to sonarqube recommandation

--- src/js/client/animation.js
+++ src/js/client/animation.js
@@ -6,20 +6,20 @@ murmures.AnimationScheduler = function () {
 
 murmures.AnimationScheduler.prototype = {
     init : function () {
-        let instance = this;
+        const instance = this;
         window.addEventListener('heroAnimationEnded', function (e) {
             instance.onHeroAnimationEnded(e.detail);
         }, false);
-        window.addEventListener('requestRenderReportQueue', function (e) {
+        window.addEventListener('requestRenderReportQueue', function () {
             instance.renderReportQueue();
         }, false);
         this.animationTick(0);
     },
     
     animationTick : function (timestamp) {
-        let instance = this;
+        const instance = this;
         if (this.animationQueue.length > 0) {
-            let newAnimationQueue = [];
+            const newAnimationQueue = [];
             document.getElementById('projectileLayer').getContext('2d').clearRect(0, 0, gameEngine.level.width * gameEngine.tileSize, gameEngine.level.height * gameEngine.tileSize);
             this.animationQueue.forEach(function (projectile) {
                 if (timestamp <= projectile.end) {
@@ -41,9 +41,9 @@ murmures.AnimationScheduler.prototype = {
     
     onHeroAnimationEnded : function () {
         if (gameEngine.reportQueue.length === 0) return;
-        let aiProjectileMoves = gameEngine.reportQueue.filter(function (report) { return report.priority === 120 });
+        const aiProjectileMoves = gameEngine.reportQueue.filter(function (report) { return report.priority === 120 });
         if (aiProjectileMoves.length > 0) {
-            let instance = this;
+            const instance = this;
             aiProjectileMoves.forEach(function (report) {
                 window.requestAnimationFrame(function (timestamp) {
                     instance.queueProjectile(timestamp + 100, report.sourceTile, report.targetTile);
@@ -55,11 +55,11 @@ murmures.AnimationScheduler.prototype = {
     
     renderReportQueue : function () {
         if (gameEngine.reportQueue.length === 0) return;
-        let instance = this;
+        const instance = this;
         gameEngine.heros.forEach(function (hero) {
             document.getElementById('hero' + hero.guid + '-box').dataset.order = '';
         }, this);
-        let heroMoves = gameEngine.reportQueue.filter(function (report) { return report.priority === 10 });
+        const heroMoves = gameEngine.reportQueue.filter(function (report) { return report.priority === 10 });
         if (heroMoves.length > 0) {
             heroMoves.forEach(function (report) {
                 window.requestAnimationFrame(function (timestamp) {
@@ -68,7 +68,7 @@ murmures.AnimationScheduler.prototype = {
             }, this);
             gameEngine.reportQueue = gameEngine.reportQueue.filter(function (report) { return report.priority !== 10 });
         }
-        let heroProjectileMoves = gameEngine.reportQueue.filter(function (report) { return report.priority === 20 });
+        const heroProjectileMoves = gameEngine.reportQueue.filter(function (report) { return report.priority === 20 });
         if (heroProjectileMoves.length > 0) {
             heroProjectileMoves.forEach(function (report) {
                 window.requestAnimationFrame(function (timestamp) {
@@ -93,14 +93,14 @@ murmures.AnimationScheduler.prototype = {
         else if (destTile.x === sourceTile.x - 1 && destTile.y === sourceTile.y) direction = 6;
         else if (destTile.x === sourceTile.x - 1 && destTile.y === sourceTile.y - 1) direction = 7;
         if (direction === -1) return; // this happens when the player moves the mouse very fast -> source and dest are more than 1 tile appart
-        let sourceBody = '_b1_91_travel_path_to' + (1 + direction).toString();
-        let destBody = '_b1_91_travel_path_from' + (1 + ((direction + 4) % 8)).toString();
-        let sourceRank = gameEngine.bodies[sourceBody].rank;
-        let destRank = gameEngine.bodies[destBody].rank;
-        let sourceX = sourceRank % 64;
-        let sourceY = (sourceRank - sourceX) / 64;
-        let destX = destRank % 64;
-        let destY = (destRank - destX) / 64;
+        const sourceBody = '_b1_91_travel_path_to' + (1 + direction).toString();
+        const destBody = '_b1_91_travel_path_from' + (1 + ((direction + 4) % 8)).toString();
+        const sourceRank = gameEngine.bodies[sourceBody].rank;
+        const destRank = gameEngine.bodies[destBody].rank;
+        const sourceX = sourceRank % 64;
+        const sourceY = (sourceRank - sourceX) / 64;
+        const destX = destRank % 64;
+        const destY = (destRank - destX) / 64;
         return {
             sourceX: sourceX,
             sourceY: sourceY,
@@ -110,7 +110,7 @@ murmures.AnimationScheduler.prototype = {
     },
     
     queueTrail : function (sourceTile, destTile, start, end) {
-        let ret = this.computeTrail(sourceTile, destTile);
+        const ret = this.computeTrail(sourceTile, destTile);
         if (typeof ret === 'undefined') return;
         this.animationQueue.push({
             start: start,
@@ -131,11 +131,11 @@ murmures.AnimationScheduler.prototype = {
     },
     
     queueProjectile : function (start, sourceTile, destTile, endEvent) {
-        let direction = -1;
-        let deltaX = destTile.x - sourceTile.x;
-        let absDeltaX = Math.abs(deltaX);
-        let deltaY = destTile.y - sourceTile.y;
-        let absDeltaY = Math.abs(deltaY);
+        const direction = -1;
+        const deltaX = destTile.x - sourceTile.x;
+        const absDeltaX = Math.abs(deltaX);
+        const deltaY = destTile.y - sourceTile.y;
+        const absDeltaY = Math.abs(deltaY);
         // Note: 2.414 === 1 / Math.tan(45°/2) ; this is the bisecting angle between 2 directions
         if (deltaY < 0 && 2.414 * absDeltaX < absDeltaY) direction = 0;
         else if (deltaX > 0 && 2.414 * absDeltaY < absDeltaX) direction = 2;
@@ -146,10 +146,10 @@ murmures.AnimationScheduler.prototype = {
         else if (deltaY > 0 && deltaX < 0) direction = 5;
         else if (deltaY < 0 && deltaX < 0) direction = 7;
         //let imgRank = gameEngine.bodies['_b1_92_flame0'].rank;
-        let imgRank = gameEngine.bodies['_b1_92_stone_arrow' + direction].rank;
+        const imgRank = gameEngine.bodies['_b1_92_stone_arrow' + direction].rank;
         //let imgRank = gameEngine.bodies['_b1_92_icicle' + direction].rank;
-        let imgX = imgRank % 64;
-        let imgY = (imgRank - imgX) / 64;
+        const imgX = imgRank % 64;
+        const imgY = (imgRank - imgX) / 64;
         this.animationQueue.push({
             start: start,
             end: start + 150 + 100 * Math.max(absDeltaX, absDeltaY),
@@ -162,9 +162,9 @@ murmures.AnimationScheduler.prototype = {
     },
     
     drawAnimation : function (start, end, timestamp, imgX, imgY, sourceTile, destTile) {
-        let lerpRatio = (timestamp - start) / (end - start);
-        let lerpX = sourceTile.x * (1 - lerpRatio) + destTile.x * lerpRatio;
-        let lerpY = sourceTile.y * (1 - lerpRatio) + destTile.y * lerpRatio;
+        const lerpRatio = (timestamp - start) / (end - start);
+        const lerpX = sourceTile.x * (1 - lerpRatio) + destTile.x * lerpRatio;
+        const lerpY = sourceTile.y * (1 - lerpRatio) + destTile.y * lerpRatio;
         document.getElementById('projectileLayer').getContext('2d').drawImage(gameEngine.client.renderer.tileset.color.imgElement,
                     imgX * gameEngine.tileSize, imgY * gameEngine.tileSize, gameEngine.tileSize, gameEngine.tileSize,
                     gameEngine.tileSize * lerpX, gameEngine.tileSize * lerpY, gameEngine.tileSize, gameEngine.tileSize);
